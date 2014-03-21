938a027dac0543c2054e1ce1d1d3d679581dca1e
Gerardo Marset <gammer1994@gmail.com>
2014-03-21 14:07:07 -0300

    Convert commands to classes.

--- haxeng/cli.py
+++ haxeng/cli.py
@@ -42,7 +42,7 @@ class CLI(object):
 
     @staticmethod
     def command(cmd):
-        CLI.commands[cmd.__name__[4:]] = cmd
+        CLI.commands[cmd.name] = cmd
 
     @property
     def pwd(self):
@@ -69,7 +69,7 @@ class CLI(object):
             args = self.game.aliases[args[0]].split(" ") + args[1:]
 
         if args[0] in self.commands:
-            self.commands[args[0]](self, args[1:])
+            self.commands[args[0]].try_to_run(self, args[1:])
         else:
             print("betch: comando desconocido: {}".format(args[0]))
 
@@ -109,20 +109,42 @@ class CLI(object):
 class Command(object):
     name = ""
     description = ""
-    syntax = ""
-    number_of_arguments = []
+    parameters = []
+    condition = (lambda cli: True, "")
+
+    @classmethod
+    def minimum_parameters(cls):
+        # !!!: It seems this can't be a property for some reason.
+        #      Probably because it's a classmethod...?
+        optional = 0
+        for parameter in cls.parameters:
+            optional += parameter[0] == "["
+        return len(cls.parameters) - optional
 
     @classmethod
     def print_help(cls):
-        print("{}: {}".format(cls.name, cls.description))
+        print("{}{}".format(cls.name.ljust(10, "."), cls.description))
+
+    @classmethod
+    def print_syntax(cls):
+        print("sintaxis:")
+        print(" ".join([cls.name] + cls.parameters))
+
+    @classmethod
+    def print_msg(cls, *error):
+        print("{}: {}".format(cls.name, " ".join(error)))
 
     @classmethod
     def try_to_run(cls, cli, args):
         if "/?" in args:
             cls.print_help()
             return
-        if cls.number_of_arguments == [] or \
-           len(args) not in cls.number_of_arguments:
+
+        if not cls.condition[0](cli):
+            cls.print_msg(cls.condition[1])
+            return
+
+        if not cls.minimum_parameters() <= len(args) <= len(cls.parameters):
             cls.print_syntax()
             return
         cls.run(cli, args)
@@ -133,298 +155,335 @@ class Command(object):
 
 
 @CLI.command
-def cmd_help(cli, args):
-    if "/?" in args:
-        print("help:      Muestra la ayuda de todos los comandos.")
-        return
+class cmd_help(Command):
+    name = "help"
+    description = "Muestra la ayuda de todos los comandos."
+    parameters = []
 
-    for command in cli.commands.values():
-        command(cli, ["/?"])
-
-
-@CLI.command
-def cmd_dir(cli, args):
-    if "/?" in args:
-        print("dir:       Muestra el contenido del directorio actual.")
-        return
-    if len(args) > 1:
-        print("sintaxis:")
-        print("dir [DIRECTORIO]")
-        return
-
-    if len(args) == 1:
-        dirlist = cli.system.abs_dirlist(cli.dirlist[:],
-                                         tools.fix_slashes(args[0]).
-                                         split(os.path.sep))
-    else:
-        dirlist = cli.dirlist[:]
-
-    elements = cli.system.listdir(dirlist)
-    directories = [directory[0] for directory in elements
-                   if not isinstance(directory[1], File)]
-    files = [directory[0] for directory in elements
-             if isinstance(directory[1], File)]
-
-    print()
-    if dirlist != cli.dirlist:
-        print("    Directorio de:", tools.format_dirlist(dirlist))
-        print()
-    for directory in sorted(directories):
-        print("{0:32} {1}".format(directory, "<DIR>"))
-    for file_ in sorted(files):
-        print(file_)
-    print()
-    print("   ", len(files), "archivo(s).")
-    print("   ", len(directories), "directorio(s).")
-    print()
+    @classmethod
+    def run(cls, cli, args):
+        for name in sorted(cli.commands):
+            cli.commands[name].print_help()
 
 
 @CLI.command
-def cmd_cd(cli, args):
-    if len(args) == 0:
-        print(cli.pwd)
-        return
-    if "/?" in args:
-        print("cd:        Cambia el directorio actual.")
-        return
-    if len(args) > 1:
-        print("sintaxis:")
-        print("cd DIRECTORIO")
-        return
+class cmd_dir(Command):
+    name = "dir"
+    description = "Muestra el contenido del directorio actual."
+    parameters = ["[DIRECTORIO]"]
 
-    relative_dirlist = tools.fix_slashes(args[0]).split(os.path.sep)
-    dirlist = cli.system.abs_dirlist(cli.dirlist[:], relative_dirlist)
-
-    if cli.system.is_file(dirlist):
-        print("cd: no es un directorio:", args[0])
-    elif not cli.system.is_directory(dirlist):
-        print("cd: archivo o directorio no encontrado:", args[0])
-    else:
-        cli.dirlist = dirlist
-
-
-@CLI.command
-def cmd_del(cli, args):
-    if "/?" in args:
-        print("del:       Borra el archivo o directorio dado.")
-        return
-    if len(args) != 1:
-        print("sintaxis:")
-        print("del ARCHIVO/DIRECTORIO")
-        return
-
-    # TODO: Remove this constraint.
-    if not DEBUG and cli.system.is_local:
-        print("del: no podés borrar cosas de tu propio sistema")
-        return
-
-    relative_dirlist = tools.fix_slashes(args[0]).split(os.path.sep)
-
-    try:
-        cli.system.delete(cli.system.abs_dirlist(cli.dirlist[:],
-                                                 relative_dirlist))
-    except KeyError:
-        print("del: archivo o directorio no encontrado:", args[0])
-        return
-
-    print("del: ok")
-
-    if not cli.system.is_directory(cli.dirlist):
-        print("del: el directorio actual fue borrado, volviendo a la raíz")
-        cli.dirlist = cli.dirlist[:1]
-
-
-@CLI.command
-def cmd_ping(cli, args):
-    if "/?" in args:
-        print("ping:      Comprueba mediante ping si un host está online.")
-        return
-    if len(args) != 1:
-        print("sintaxis:")
-        print("ping HOST")
-        return
-
-    ip = args[0]
-    valid = ip in cli.game.valid_hosts
-    print()
-    print("Haciendo ping a {} con 32 bytes de datos:".format(ip))
-    print()
-    for i in range(4):
-        time.sleep(.5)
-        if valid:
-            print("Respuesta desde {}: bytes=32 tiempo<1m TTL=64".format(ip))
+    @classmethod
+    def run(cls, cli, args):
+        if len(args) == 1:
+            dirlist = cli.system.abs_dirlist(cli.dirlist[:],
+                                             tools.fix_slashes(args[0]).
+                                             split(os.path.sep))
         else:
-            print("Tiempo de espera agotado para esta solicitud.")
-    print()
-    print("Estadísticas de ping para {}:".format(ip))
-    print("    Paquetes: enviados = 4, recibidos = {}, perdidos = {}".
-          format(valid * 4, 4 - valid * 4))
-    print("    ({}% perdidos),".format(100 - valid * 100))
-    print()
+            dirlist = cli.dirlist[:]
 
+        try:
+            elements = cli.system.listdir(dirlist)
+        except KeyError:
+            cls.print_msg("archivo o directorio no encontrado:", args[0])
+            return
+        except AttributeError:
+            cls.print_msg("no es un directorio:", args[0])
+            return
 
-@CLI.command
-def cmd_telnet(cli, args):
-    if "/?" in args:
-        print("ping:      Comprueba mediante ping si un host está online.")
-        return
-    if len(args) == 0 or len(args) > 2:
-        print("sintaxis:")
-        print("telnet HOST [PUERTO]")
-        return
+        directories = [directory[0] for directory in elements
+                       if not isinstance(directory[1], File)]
+        files = [directory[0] for directory in elements
+                 if isinstance(directory[1], File)]
 
-    host = args[0]
-    port = args[1] if len(args) > 1 else 23
-    print("Conectándose a {}".format(host), end="", flush=True)
-    #time.sleep(1.5)
-    for i in range(3):
-        print(".", end="", flush=True)
-        #time.sleep(1.5)
-    print()
-
-    if host != cli.game.mission.system.ip or \
-       port != cli.game.mission.port:
-        print("telnet: no se puede abrir la conexión al host, en puerto {}".
-              format(port))
-        #return
-
-    cli.clear()
-    for i in range(random.randint(10000, 30000)):
-        print(random.randint(0, 1), end=" ", flush=True)
-        time.sleep(0.00001)
-    print()
-    cli.game.telnet_start()
-
-
-@CLI.command
-def cmd_cls(cli, args):
-    if "/?" in args:
-        print("cls:       Borra el texto en la pantalla.")
-        return
-
-    cli.clear()
-
-
-@CLI.command
-def cmd_dl(cli, args):
-    if "/?" in args:
-        print("dl:        Descarga un archivo durante una sesión de telnet.")
-        return
-    if len(args) != 1:
-        print("sintaxis:")
-        print("dl ARCHIVO")
-        return
-
-
-@CLI.command
-def cmd_ul(cli, args):
-    pass
-
-
-@CLI.command
-def cmd_info(cli, args):
-    pass
-
-
-@CLI.command
-def cmd_mail(cli, args):
-    if "/?" in args:
-        print("mail:      Muestra el último e-mail recibido.")
-        return
-    if not cli.system.is_local:
-        print("mail: no hay e-mails nuevos")
         print()
-        return
-
-    email = cli.game.mission.email
-    print(" " + "_" * (TERM_WIDTH - 2))
-    print("|{}|".format("De: {} ({})".
-                        format(email.sender, email.sender_email).
-                        center(TERM_WIDTH - 2)))
-    print("|" + "-" * (TERM_WIDTH - 2) + "|")
-    print("|{}|".format("Asunto: {}".
-                        format(email.subject).center(TERM_WIDTH - 2)))
-    print(" " + "¯" * (TERM_WIDTH - 2))
-    print(tools.formatted(email.message, TERM_WIDTH))
-    print("-" * TERM_WIDTH)
+        if dirlist != cli.dirlist:
+            print("    Directorio de:", tools.format_dirlist(dirlist))
+            print()
+        for directory in sorted(directories):
+            print("{0:32} {1}".format(directory, "<DIR>"))
+        for file_ in sorted(files):
+            print(file_)
+        print()
+        print("   ", len(files), "archivo(s).")
+        print("   ", len(directories), "directorio(s).")
+        print()
 
 
 @CLI.command
-def cmd_dc(cli, args):
-    if "/?" in args:
-        print("dc:        Termina una sesión de telnet.")
-        return
+class cmd_cd(Command):
+    name = "cd"
+    description = "Cambia el directorio actual."
+    parameters = ["[DIRECTORIO]"]
 
-    if cli.system.is_local:
-        print("dc: no estás en una sesión de telnet")
-        return
+    @classmethod
+    def run(cls, cli, args):
+        if len(args) == 0:
+            print(cli.pwd)
+            return
 
-    cli.game.telnet_end()
+        relative_dirlist = tools.fix_slashes(args[0]).split(os.path.sep)
+        dirlist = cli.system.abs_dirlist(cli.dirlist[:], relative_dirlist)
 
-
-@CLI.command
-def cmd_save(cli, args):
-    if "/?" in args:
-        print("save:      Guarda la partida.")
-        return
-
-    cli.game.save()
-    print("save: partida guardada correctamente")
-
-
-@CLI.command
-def cmd_track(cli, args):
-    pass
+        if cli.system.is_file(dirlist):
+            cls.print_msg("no es un directorio:", args[0])
+        elif not cli.system.is_directory(dirlist):
+            cls.print_msg("archivo o directorio no encontrado:", args[0])
+        else:
+            cli.dirlist = dirlist
 
 
 @CLI.command
-def cmd_ipconfig(cli, args):
-    if "/?" in args:
-        print("ipconfig:  Muestra tu configuración de IP.")
-        return
-    ip = cli.system.ip
+class cmd_del(Command):
+    name = "del"
+    description = "Borra el archivo o directorio dado."
+    parameters = ["ARCHIVO/DIRECTORIO"]
+    # TODO: Remove this constraint.
+    condition = (lambda cli: not cli.system.is_local,
+                 "no podés borrar cosas de tu propio sistema")
 
-    print()
-    print("Configuración IP:")
-    print()
-    print("        Sufijo de conexión específica DNS :")
-    print("        Dirección IP. . . . . . . . . . . :", ip)
-    print("        Máscara de subred . . . . . . . . : 255.255.255.255")
-    print("        Puerta de enlace predeterminada . :", ip)
-    print()
+    @classmethod
+    def run(cls, cli, args):
+        relative_dirlist = tools.fix_slashes(args[0]).split(os.path.sep)
 
+        try:
+            cli.system.delete(cli.system.abs_dirlist(cli.dirlist[:],
+                                                     relative_dirlist))
+        except KeyError:
+            cls.print_msg("archivo o directorio no encontrado:", args[0])
+            return
 
-@CLI.command
-def cmd_alias(cli, args):
-    if "/?" in args:
-        print("alias:     Crea un alias a un comando.")
-        return
-    if len(args) == 1:
-        print("sintaxis:")
-        print("alias NOMBRE COMANDO")
-        return
-    if len(args) == 0:
-        for name, command in sorted(cli.game.aliases.items()):
-            print("{}='{}'".format(name, command))
-        return
+        cls.print_msg("ok")
 
-    cli.game.aliases[args[0]] = " ".join(args[1:])
-    print("alias: ok")
+        if not cli.system.is_directory(cli.dirlist):
+            cls.print_error("el directorio actual fue borrado, "
+                            "volviendo a la raíz")
+            cli.dirlist = cli.dirlist[:1]
 
 
 @CLI.command
-def cmd_unalias(cli, args):
-    if "/?" in args:
-        print("unalias:   Elimina un alias.")
-        return
-    if len(args) != 1:
-        print("sintaxis:")
-        print("unalias NOMBRE")
-        return
+class cmd_ping(Command):
+    name = "ping"
+    description = "Comprueba mediante ping si un host está online."
+    parameters = ["HOST"]
 
-    try:
-        del cli.game.aliases[args[0]]
-    except KeyError:
-        print("unalias: alias no encontrado:", args[0])
-    else:
-        print("unalias: ok")
+    @classmethod
+    def run(cls, cli, args):
+        ip = args[0]
+        valid = ip in cli.game.valid_hosts
+        print()
+        print("Haciendo ping a {} con 32 bytes de datos:".format(ip))
+        print()
+        for i in range(4):
+            time.sleep(.5)
+            if valid:
+                print("Respuesta desde {}: bytes=32 tiempo<1m TTL=64".
+                      format(ip))
+            else:
+                print("Tiempo de espera agotado para esta solicitud.")
+        print()
+        print("Estadísticas de ping para {}:".format(ip))
+        print("    Paquetes: enviados = 4, recibidos = {}, perdidos = {}".
+              format(valid * 4, 4 - valid * 4))
+        print("    ({}% perdidos),".format(100 - valid * 100))
+        print()
+
+
+@CLI.command
+class cmd_telnet(Command):
+    name = "telnet"
+    description = "Inicializa una sesión telnet con un host."
+    parameters = ["HOST", "[PUERTO]"]
+    condition = (lambda cli: cli.system.is_local,
+                 "ya hay una sesión telnet iniciada")
+
+    @classmethod
+    def run(cls, cli, args):
+        host = args[0]
+        port = args[1] if len(args) > 1 else 23
+        print("Conectándose a {}".format(host), end="", flush=True)
+        #time.sleep(1.5)
+        for i in range(3):
+            print(".", end="", flush=True)
+            #time.sleep(1.5)
+        print()
+
+        if host != cli.game.mission.system.ip or \
+           port != cli.game.mission.port:
+            cls.print_msg("no se puede abrir la conexión al host, "
+                          "en puerto {}".format(port))
+            return
+
+        cli.clear()
+        for i in range(random.randint(10000, 30000)):
+            print(random.randint(0, 1), end=" ", flush=True)
+            time.sleep(0.00001)
+        print()
+        cli.game.telnet_start()
+
+
+@CLI.command
+class cmd_cls(Command):
+    name = "cls"
+    description = "Borra el texto en la pantalla."
+    parameters = []
+
+    @classmethod
+    def run(cls, cli, args):
+        cli.clear()
+
+
+@CLI.command
+class cmd_dl(Command):
+    name = "dl"
+    description = "Descarga un archivo durante una sesión de telnet."
+    parameters = ["ARCHIVO"]
+    condition = (lambda cli: not cli.system.is_local,
+                 "no estás en una sesión telnet")
+
+    @classmethod
+    def run(cls, cli, args):
+        pass
+
+
+@CLI.command
+class cmd_ul(Command):
+    name = "ul"
+    description = ""
+    parameters = []
+    condition = (lambda cli: not cli.system.is_local,
+                 "no estás en una sesión telnet")
+
+    @classmethod
+    def run(cls, cli, args):
+        pass
+
+
+@CLI.command
+class cmd_info(Command):
+    name = "info"
+    description = ""
+    parameters = []
+
+    @classmethod
+    def run(cls, cli, args):
+        pass
+
+
+@CLI.command
+class cmd_mail(Command):
+    name = "mail"
+    description = "Muestra el último e-mail recibido."
+    parameters = []
+
+    @classmethod
+    def run(cls, cli, args):
+        if not cli.system.is_local:
+            cls.print_msg("no hay e-mails nuevos")
+            print()
+            return
+
+        email = cli.game.mission.email
+        print(" " + "_" * (TERM_WIDTH - 2))
+        print("|{}|".format("De: {} ({})".
+                            format(email.sender, email.sender_email).
+                            center(TERM_WIDTH - 2)))
+        print("|" + "-" * (TERM_WIDTH - 2) + "|")
+        print("|{}|".format("Asunto: {}".
+                            format(email.subject).center(TERM_WIDTH - 2)))
+        print(" " + "¯" * (TERM_WIDTH - 2))
+        print(tools.formatted(email.message, TERM_WIDTH))
+        print("-" * TERM_WIDTH)
+
+
+@CLI.command
+class cmd_dc(Command):
+    name = "dc"
+    description = "Termina una sesión de telnet."
+    parameters = []
+    condition = (lambda cli: not cli.system.is_local,
+                 "no estás en una sesión telnet")
+
+    @classmethod
+    def run(cls, cli, args):
+        cli.game.telnet_end()
+
+
+@CLI.command
+class cmd_save(Command):
+    name = "save"
+    description = "Guarda la partida."
+    parameters = []
+
+    @classmethod
+    def run(cls, cli, args):
+        cli.game.save()
+        cli.print_msg("partida guardada correctamente")
+
+
+@CLI.command
+class cmd_track(Command):
+    name = "track"
+    description = ""
+    parameters = []
+
+    @classmethod
+    def run(cls, cli, args):
+        pass
+
+
+@CLI.command
+class cmd_ipconfig(Command):
+    name = "ipconfig"
+    description = "Muestra tu configuración de IP."
+    parameters = []
+
+    @classmethod
+    def run(cls, cli, args):
+        print()
+        print("Configuración IP:")
+        print()
+        print("        Sufijo de conexión específica DNS :")
+        print("        Dirección IP. . . . . . . . . . . :", cli.system.ip)
+        print("        Máscara de subred . . . . . . . . : 255.255.255.255")
+        print("        Puerta de enlace predeterminada . :", cli.system.ip)
+        print()
+
+
+@CLI.command
+class cmd_alias(Command):
+    name = "alias"
+    description = "Crea un alias a un comando."
+    parameters = ["[NOMBRE]", "[COMANDO]"]
+
+    @classmethod
+    def run(cls, cli, args):
+        if len(args) == 0:
+            for name, command in sorted(cli.game.aliases.items()):
+                print("{}='{}'".format(name, command))
+            return
+
+        if len(args) == 1:
+            if args[0] not in cli.game.aliases:
+                cls.print_msg("alias no encontrado:", args[0])
+                return
+            print("{}='{}'".format(args[0], cli.game.aliases[args[0]]))
+            return
+
+        cli.game.aliases[args[0]] = " ".join(args[1:])
+        cls.print_msg("ok")
+
+
+@CLI.command
+class cmd_unalias(Command):
+    name = "unalias"
+    description = "Elimina un alias."
+    parameters = ["NOMBRE"]
+
+    @classmethod
+    def run(cls, cli, args):
+        try:
+            del cli.game.aliases[args[0]]
+        except KeyError:
+            cls.print_msg("alias no encontrado:", args[0])
+        else:
+            cls.print_msg("ok")
