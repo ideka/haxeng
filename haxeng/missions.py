7ee26ba5a85492a1c4780ceccf49df79f7e318cc
Gerardo Marset <gammer1994@gmail.com>
2014-03-21 17:41:34 -0300

    Fix GPL notice.

--- haxeng/missions.py
+++ haxeng/missions.py
@@ -2,18 +2,18 @@
 #
 #       This file is part of Haxxor Engine.
 #
-#       system.py is free software; you can redistribute it and/or modify
+#       Haxxor Engine is free software; you can redistribute it and/or modify
 #       it under the terms of the GNU General Public License as published by
 #       the Free Software Foundation; either version 3 of the License, or
 #       (at your option) any later version.
 #
-#       system.py is distributed in the hope that it will be useful,
+#       Haxxor Engine is distributed in the hope that it will be useful,
 #       but WITHOUT ANY WARRANTY; without even the implied warranty of
 #       MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #       GNU General Public License for more details.
 #
 #       You should have received a copy of the GNU General Public License
-#       along with system.py; if not, write to the Free Software
+#       along with Haxxor Engine; if not, write to the Free Software
 #       Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #       MA 02110-1301, USA.
 # }}}
